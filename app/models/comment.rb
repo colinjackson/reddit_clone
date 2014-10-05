1782496a029eb13b080d5fee42e69ad81d94b055
Colin Jackson <colin.s.jackson90@gmail.com>
2014-10-05 02:55:51 -0400

    Add nested comments model logic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,14 @@ class Comment < ActiveRecord::Base
   validates_presence_of :author, :post
 
   belongs_to :author, class_name: 'User', inverse_of: :comments
-
   belongs_to :post, inverse_of: :comments
+
+  belongs_to :parent_comment,
+  class_name: 'Comment',
+  inverse_of: :child_comments
+
+  has_many :child_comments,
+  class_name: 'Comment',
+  foreign_key: :parent_comment_id,
+  inverse_of: :parent_comment
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,9 @@ class Post < ActiveRecord::Base
   belongs_to :author, class_name: "User", inverse_of: :posts
   has_many :sub_posts, inverse_of: :post
   has_many :subs, through: :sub_posts, source: :sub
+
   has_many :comments, inverse_of: :post
+  has_many :top_level_comments,
+  -> { where("parent_comment_id IS NULL") },
+  class_name: 'Comment'
 end
